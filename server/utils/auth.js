1842f2075dbbf27dc18ff7df7b4f4afa45b0f2e6
Kristi Rowland <157322992+gocrushgoals@users.noreply.github.com>
2024-07-18 19:24:13 -0400

    Revert "Seeder Removed & Dot Env Added"

--- server/utils/auth.js
+++ server/utils/auth.js
@@ -1,7 +1,7 @@
 const jwt = require('jsonwebtoken');
 
 // set token secret and expiration date
-const secret = process.env.SECRET_KEY;
+const secret = 'mysecretsshhhhh';
 const expiration = '2h';
 
 module.exports = {
